f7bc7969e7360931cabe7ddb41280d9f35825c9b
H. Walid <h.walid@hexaee.com>
2024-03-03 20:04:12 +0100

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Login_Register_Identity_.NET8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
