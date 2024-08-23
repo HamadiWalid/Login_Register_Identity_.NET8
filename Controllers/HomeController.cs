dd359f8c0a950b00a7ce171cca65f2c3ded67e6d
H. Walid <h.walid@hexaee.com>
2024-08-23 23:15:35 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using Login_Register_Identity_.NET8.Areas.Identity.Data;
+using Login_Register_Identity_.NET8.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Login_Register_Identity_.NET8.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger,UserManager<ApplicationUser>userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["UserID"] = _userManager.GetUserId(this.User);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
